6ffc9487a696776b9242965d7b20ab7adbceaa26
info@codewithjalloh.com <supremejalloh>
2016-06-26 21:02:19 +0100

    outlet and action added

--- myMapKit/ViewController.swift
+++ myMapKit/ViewController.swift
@@ -9,17 +9,41 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet weak var lat: UILabel!
+    @IBOutlet weak var long: UILabel!
+    @IBOutlet weak var myMapView: MKMapView!
+    
+    
+    var set = NSMutableArray()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       self.myMapView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func createAnno(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func deleteAnno(sender: AnyObject) {
+        
+    }
+    
+    
+    
+    
+    
+    
 
 
 }
